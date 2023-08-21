1f34d93d6aeda6966f22496afec3dcd3efc4c262
66643_Abhijeet <abhijawale12@gmail.com>
2023-08-22 00:56:32 +0530

    Abhijeet first commit

---
+++ src/main/java/com/soccer/stars/model/Ground.java
@@ -0,0 +1,80 @@
+package com.soccer.stars.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+@Entity
+public class Ground {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer ground_id;
+	@JsonIgnore
+	@OneToMany(mappedBy="ground", fetch=FetchType.LAZY)
+	private List<BookingSlot> slots;
+	private Long capacity;
+	private Double size;
+	private String name;
+	@ManyToOne
+	private Club club;
+	
+	
+	public Club getClub() {
+		return club;
+	}
+	public void setClub(Club club) {
+		this.club = club;
+	}
+	public Integer getGround_id() {
+		return ground_id;
+	}
+	public void setGround_id(Integer ground_id) {
+		this.ground_id = ground_id;
+	}
+	public List<BookingSlot> getSlots() {
+		return slots;
+	}
+	public void setSlots(List<BookingSlot> slots) {
+		this.slots = slots;
+	}
+	public Long getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(Long capacity) {
+		this.capacity = capacity;
+	}
+	public Double getSize() {
+		return size;
+	}
+	public void setSize(Double size) {
+		this.size = size;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Ground(Integer ground_id, List<BookingSlot> slots, Long capacity, Double size, String name, Club club) {
+		super();
+		this.ground_id = ground_id;
+		this.slots = slots;
+		this.capacity = capacity;
+		this.size = size;
+		this.name = name;
+		this.club = club;
+	}
+	public Ground() {
+		super();
+	}
+	
+	
+}
---
+++ src/main/java/com/soccer/stars/model/Player.java
@@ -0,0 +1,100 @@
+package com.soccer.stars.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Player {
+	@OneToOne
+	private Users user;
+	@Id
+	@Column(nullable = false, unique = true)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer pid;
+	@OneToOne
+	private Coach personalCoach;
+	@OneToOne
+	private Booking booking;
+	@OneToOne
+	private Club club;
+
+	@JsonIgnore
+	@ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
+	private TrainingGroup group;
+
+
+
+	public Player(Users user, Integer pid, Coach personalCoach, Booking booking, Club club, TrainingGroup group) {
+		super();
+		this.user = user;
+		this.pid = pid;
+		this.personalCoach = personalCoach;
+		this.booking = booking;
+		this.club = club;
+		this.group = group;
+	}
+
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public Coach getPersonalCoach() {
+		return personalCoach;
+	}
+
+	public void setPersonalCoach(Coach personalCoach) {
+		this.personalCoach = personalCoach;
+	}
+
+	public Booking getBooking() {
+		return booking;
+	}
+
+	public void setBooking(Booking booking) {
+		this.booking = booking;
+	}
+
+	public Club getClub() {
+		return club;
+	}
+
+	public void setClub(Club club) {
+		this.club = club;
+	}
+
+
+	public TrainingGroup getGroup() {
+		return group;
+	}
+
+	public void setGroup(TrainingGroup group) {
+		this.group = group;
+	}
+
+	public Player() {
+		super();
+	}
+
+}
