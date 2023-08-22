cfb41eec5a8670638484e6ee1685f6e5868ce240
Gaurav Pal <g1aurav.org114gmail.com>
2023-08-22 22:02:31 +0530

    2commit

--- src/main/java/com/stars/soccer/service/TrainingGroupService.java
+++ src/main/java/com/stars/soccer/service/TrainingGroupService.java
@@ -1,4 +1,4 @@
-package com.stars.soccer.service;
+package com.pro.soccer.service;
 
 import java.util.List;
 import java.util.Optional;
@@ -6,35 +6,35 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.pro.soccer.model.Ground;
-import com.pro.soccer.repo.GroundRepo;
+import com.pro.soccer.model.TrainingGroup;
+import com.pro.soccer.repo.TrainingGroupRepo;
 
 @Service
-public class GroundService implements CrudService<Ground, Integer>{
+public class TrainingGroupService implements CrudService<TrainingGroup, Integer>{
 
 	@Autowired
-	GroundRepo repo;
+	TrainingGroupRepo repo;
 	
 	@Override
-	public void add(Ground t) {
+	public void add(TrainingGroup t) {
 		// TODO Auto-generated method stub
 		repo.save(t);
 	}
 
 	@Override
-	public Ground getById(Integer id) {
+	public TrainingGroup getById(Integer id) {
 		// TODO Auto-generated method stub
 		return (repo.findById(id).equals(Optional.empty()))?null:repo.findById(id).get();
 	}
 
 	@Override
-	public List<Ground> getAll() {
+	public List<TrainingGroup> getAll() {
 		// TODO Auto-generated method stub
 		return repo.findAll();
 	}
 
 	@Override
-	public void update(Ground t) {
+	public void update(TrainingGroup t) {
 		// TODO Auto-generated method stub
 		repo.save(t);
 	}
